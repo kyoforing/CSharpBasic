1e8adb40c1e88fc58fb9f139e4e3e7ddb6a1aad4
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2020-04-24 11:26:55 +0800

    clean up code

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using CSharpBasic.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using CSharpBasic.Models;
+using System.Diagnostics;
 
 namespace CSharpBasic.Controllers
 {
