d603c2214ed7ffca31e1931a640f4a7ff55ff2b3
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2020-04-28 15:37:22 +0800

    Add Ms test project

---
+++ CSharpBasicTests/Controllers/WhereAmIControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharpBasic.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpBasic.Controllers.Tests
+{
+    [TestClass()]
+    public class WhereAmIControllerTests
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
