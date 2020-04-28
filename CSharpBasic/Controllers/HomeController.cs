f4d36af86ad0d34e19a6054791e97ac5b2191090
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2020-04-28 15:26:12 +0800

    Change Folder Structure, for auto gen unit test

--- CSharpBasic/Controllers/HomeController.cs
+++ CSharpBasic/Controllers/HomeController.cs
@@ -1,7 +1,11 @@
-﻿using CSharpBasic.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System.Diagnostics;
+using CSharpBasic.Models;
 
 namespace CSharpBasic.Controllers
 {
