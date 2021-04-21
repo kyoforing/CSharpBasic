255c8eea230acea0a039786cd71baeb7df39fbcb
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2021-04-22 00:26:50 +0800

    tmp

--- CSharpBasic/Services/GeoIpService.cs
+++ CSharpBasic/Services/GeoIpService.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace CSharpBasic.Services
